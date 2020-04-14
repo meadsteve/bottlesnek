ee1fa3fe73461d1f0d6114db3b98d93d196fe1e9
steve <meadsteve@gmail.com>
2020-04-14 22:04:12 +0200

    up is down. down is up. and time is a closed spiral

--- src/main/kotlin/com/meadsteve/bottlesnek/space/space.kt
+++ src/main/kotlin/com/meadsteve/bottlesnek/space/space.kt
@@ -11,16 +11,16 @@ data class Vector(val x: Int, val y: Int)
 fun randomDirection() = Direction.values().asList().shuffled().first()
 
 enum class Direction(val value: String, val vector: Vector) {
-    Up("up", Vector(0, -1)),
-    Down("down", Vector(0, 1)),
+    Up("up", Vector(0, 1)),
+    Down("down", Vector(0, -1)),
     Left("left", Vector(-1, 0)),
     Right("right", Vector(1, 0));
 }
 
 fun findHeading(from: GridPoint, to: GridPoint): Direction {
     return when {
-        from.x == to.x && from.y < to.y -> Direction.Down
-        from.x == to.x && from.y > to.y -> Direction.Up
+        from.x == to.x && from.y > to.y -> Direction.Down
+        from.x == to.x && from.y < to.y -> Direction.Up
         from.y == to.y && from.x > to.x -> Direction.Left
         from.y == to.y && from.x < to.x -> Direction.Right
         else -> randomDirection()
