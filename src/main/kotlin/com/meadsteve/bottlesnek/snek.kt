2dac89f318f6a0d84981b623c0cb65d1d13a68bc
steve <meadsteve@gmail.com>
2020-04-14 21:54:09 +0200

    get or post for ping

--- src/main/kotlin/com/meadsteve/bottlesnek/snek.kt
+++ src/main/kotlin/com/meadsteve/bottlesnek/snek.kt
@@ -18,6 +18,7 @@ fun main(_args: Array<String>) {
 
     app.get("/") { ctx -> ctx.result("Hello snek") }
 
+    app.get("/ping") { ctx -> ctx.result("pong") }
     app.post("/ping") { ctx -> ctx.result("pong") }
 
     app.post("/start") { ctx -> ctx.json(snakeConfig)}
