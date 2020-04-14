2b73b37f7ff288dea781b80af606139a2a64d363
steve <meadsteve@gmail.com>
2020-04-14 21:01:08 +0200

    remove hardcoded leftness

--- src/main/kotlin/com/meadsteve/bottlesnek/strategies/IdealMove.kt
+++ src/main/kotlin/com/meadsteve/bottlesnek/strategies/IdealMove.kt
@@ -7,7 +7,6 @@ import com.meadsteve.bottlesnek.space.findHeading
 import com.meadsteve.bottlesnek.space.randomDirection
 
 fun idealMove(game: Game): Direction {
-    return Direction.Left
     logger.info("Trying to find ideal move")
     if(game.board.food.isEmpty()) {
         logger.info("Empty board so I'm moving at random")
